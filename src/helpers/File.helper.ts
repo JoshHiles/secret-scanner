c7c4ccb4b6107f6cf08ecf86f28a09f3ed7daec5
Joshua Hiles <joshhiles1@live.co.uk>
2021-10-16 16:45:59 +0100

    perf(defaultIgnore): Add extra default ignore folders
    Add .nyc_output & .git

--- src/helpers/File.helper.ts
+++ src/helpers/File.helper.ts
@@ -14,6 +14,8 @@ export default class FileHelper {
         '**/yarn.lock',
         '**/detect-secret-baseline.json',
         '**/.secret-scannerrc',
+        '**/.nyc_output',
+        '**/.git',
     ];
 
     constructor(configuration: Configuration) {
