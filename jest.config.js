63aa01a614e6b07d894ecb86c1975c15edfaf480
Joshua Hiles <joshhiles1@live.co.uk>
2021-11-02 17:28:08 +0000

    hardcode tsconfig for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+    globals: { 'ts-jest': { tsconfig: './tsconfig.json' } },
     preset: 'ts-jest',
     testEnvironment: 'node',
     coverageThreshold: {
