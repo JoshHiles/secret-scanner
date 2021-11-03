3799bd94eb41c4b53f8d273e6942dacb7be37c16
Joshua Hiles <joshhiles1@live.co.uk>
2021-11-03 15:17:55 +0000

    Add module dirs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     roots: ['./src', './tests'],
+    moduleDirectories: ['node_modules', 'src'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     coverageThreshold: {
