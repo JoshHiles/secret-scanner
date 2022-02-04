ea43dd61f4d1b93015772929416d172f0a5dd9e2
Joshua Hiles <joshhiles1@live.co.uk>
2022-02-04 17:28:04 +0000

    fix(PluginHelper): Change import based on js or ts plugin

--- src/helpers/Plugin.Helper.ts
+++ src/helpers/Plugin.Helper.ts
@@ -46,13 +46,20 @@ export default class PluginHelper {
     /* istanbul ignore next */
     async InitialisePlugin(pluginPath: string): Promise<Plugin> {
         const pluginName = parse(pluginPath).name;
+        const pluginExt = parse(pluginPath).ext;
 
         let pluginClass;
         // default plugins come through as 'AWS.ts' for example
         // where as user defined plugins come through as 'D:\\whatever\\custom-plugin.ts'
         // seems hacky to use includes('\\') but it works
         if (pluginPath.includes('\\')) {
-            pluginClass = await tsImport.compile(pluginPath);
+            if (pluginExt === '.js') {
+                pluginClass = await import(pluginPath);
+            } else if (pluginExt === '.ts') {
+                pluginClass = await tsImport.compile(pluginPath);
+            } else {
+                console.error(chalk.red(`${pluginName} HAS to be either .js or .ts`));
+            }
         } else {
             pluginClass = await import(path.resolve(__dirname, '../', 'plugins', pluginName));
         }
