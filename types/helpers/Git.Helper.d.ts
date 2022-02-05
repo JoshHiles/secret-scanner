0f3631920b7a31cc29818df113b0ffc1c2dbe153
Joshua Hiles <joshhiles1@live.co.uk>
2022-02-05 13:03:57 +0000

    fix(types): Add types to dist

--- types/helpers/Git.Helper.d.ts
+++
@@ -1,3 +0,0 @@
-export default class GitHelper {
-    GetStagedChanges(): Promise<string[]>;
-}
--- types/plugins/Keyword.d.ts
+++
@@ -1,8 +0,0 @@
-import { FileType } from '../interfaces/FileType.enum';
-import Plugin, { ExampleMatchType } from '../interfaces/Plugin';
-export default class Keyword implements Plugin {
-    Name: string;
-    Regexes: RegExp[];
-    ExampleMatches: ExampleMatchType;
-    Initialise(fileType: FileType): void;
-}
